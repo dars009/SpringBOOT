68888d963c4b63aa0343f608ba8f6ce479950c30
darsh <darsh@192.168.2.3>
2020-08-04 09:39:19 +0530

    SpringBOOT_02_LoggerAndActuator Project Share

---
+++ SpringBOOT_02_LoggerAndActuator/src/main/java/com/example/SpringBoot02LoggerAndActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02LoggerAndActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot02LoggerAndActuatorApplication.class, args);
+	}
+
+}
---
+++ SpringBOOT_02_LoggerAndActuator/src/main/java/com/example/beans/Employee.java
@@ -0,0 +1,56 @@
+package com.example.beans;
+
+public class Employee {
+	
+	private Integer id;
+	private String name;
+	private String city;
+	private String phone;
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(Integer id, String name, String city, String phone) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.city = city;
+		this.phone = phone;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+}
