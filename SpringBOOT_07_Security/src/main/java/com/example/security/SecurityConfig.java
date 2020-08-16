c2e1d331a3c6c74f85d346f9060f9f27f5fdd735
darsh <darsh@192.168.2.4>
2020-08-16 19:33:04 +0530

    Working Code Error Resolve for Bean Creation in Spring Boot Security Password Configuration

--- SpringBOOT_07_Security/src/main/java/com/example/security/SecurityConfig.java
+++ SpringBOOT_07_Security/src/main/java/com/example/security/SecurityConfig.java
@@ -1,37 +1,28 @@
 package com.example.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-		.antMatchers("/customer").hasAnyRole("ADMIN")
-		.anyRequest()
-		.authenticated()
-		.and()
-		//.httpBasic();
-		.formLogin();
+		http.csrf().disable().authorizeRequests().antMatchers("/customer").hasAnyRole("ADMIN").anyRequest()
+				.authenticated().and().httpBasic();
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password(encoder().encode("admin")).roles("ADMIN");
-	}
-	
-	@Bean
-	public PasswordEncoder encoder() {
-		return new BCryptPasswordEncoder();
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("admin")).roles("ADMIN");
+
 	}
 
 }
