45b6001c83f42f7a1d488356359c11183243204d
darsh <darsh@DESKTOP-4POG3SJ.bbrouter>
2022-07-30 10:51:27 +0530

    US#909090 Added new changes

--- SpringBOOT_01_Demo/src/main/java/com/example/DemoController.java
+++ SpringBOOT_01_Demo/src/main/java/com/example/DemoController.java
@@ -4,6 +4,11 @@ public class DemoController {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println(6);
+		System.out.println(7);
+		System.out.println(8);
+		System.out.println(9);
+		System.out.println(10);
 	}
 
 }
