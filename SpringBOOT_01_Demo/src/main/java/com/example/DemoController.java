e74fffbf378b6a8ccbc93a5f1e842ca541046e65
Darshit Rabadiya <darshit.rabadiya@hotmail.com>
2022-07-30 10:48:17 +0530

    Update DemoController.java
    US#44456 New changes added

--- SpringBOOT_01_Demo/src/main/java/com/example/DemoController.java
+++ SpringBOOT_01_Demo/src/main/java/com/example/DemoController.java
@@ -4,6 +4,11 @@ public class DemoController {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
 	}
 
 }
